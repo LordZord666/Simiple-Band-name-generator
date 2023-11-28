5725cccfbaeaf482a2587e9ecafea9cb3c2de7b4
Ashutosh Maharjan <70703500+LordZord666@users.noreply.github.com>
2023-11-28 13:51:48 +0545

    Add files via upload

---
+++ Bandnamegn.py
@@ -0,0 +1,6 @@
+print("Welcome to Band name generator\n".upper())
+
+cityname = input('City Name: ')
+PetName = input("Pet Name: ")
+
+print (f"Your band name would be {cityname.capitalize()} {PetName.capitalize()}")
